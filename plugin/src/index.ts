629e3d80d4878c40ecc2bdb3cedb51431ede4930
Matt Sain <matt@vervesuper.com.au>
2023-05-17 12:30:58 +1000

    fix: keywords and cleanup

--- plugin/src/index.ts
+++ plugin/src/index.ts
@@ -2,18 +2,14 @@ import { ConfigPlugin, withInfoPlist } from "@expo/config-plugins";
 
 type SKAdNetworks = string[];
 
-export const createSKAdNetworkItems = (networks: string[]) => {
-  const items = networks.length ? networks : [];
-  return items.map((id) => ({ SKAdNetworkIdentifier: id }));
-};
-
 const withSKAdNetworks: ConfigPlugin<{ skadnetworks: SKAdNetworks }> = (
   config,
   { skadnetworks = [] }
 ) => {
   config = withInfoPlist(config, (infoPlistConfig) => {
-    infoPlistConfig.modResults.SKAdNetworkItems =
-      createSKAdNetworkItems(skadnetworks);
+    infoPlistConfig.modResults.SKAdNetworkItems = skadnetworks.map(
+      (SKAdNetworkIdentifier) => ({ SKAdNetworkIdentifier })
+    );
     return infoPlistConfig;
   });
   return config;
